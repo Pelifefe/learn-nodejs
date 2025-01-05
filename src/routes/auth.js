0e801766a6106d978137a69bffb403fd41496502
Pelifefe <felippeapedrosa2004@gmail.com>
2025-01-05 07:55:17 -0300

    Resources clients: look client modal

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
-import { pool } from '../config/database.js';  // Atualizado para importar o pool de conexões do MySQL
+import { pool } from '../config/database.js';
 
 const router = express.Router();
 
